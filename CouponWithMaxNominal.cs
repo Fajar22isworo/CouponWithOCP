274a5955745777aa8da818cfde3eddf010e6fca5
Fajar22isworo <fjrisworo@gmail.com>
2020-12-13 13:38:16 +0700

    Add project files.

---
+++ CouponWithMaxNominal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithOCP
+{
+    class CouponWithMaxNominal : Coupon
+    {
+        public double discPercentage = 0;
+        public double discNominal = 0;
+        private double originPrice;
+
+        public CouponWithMaxNominal(double discPercentage, double discNominal)
+        {
+            this.discPercentage = discPercentage;
+            this.discNominal = discNominal;
+        }
+        public override double calculate (double originPice)
+        {
+            double discPercentageInRupiah = discPercentage * originPrice / 100;
+            double finalPrice = 0;
+            if (discPercentageInRupiah > discNominal) 
+            {
+                finalPrice = originPrice - discNominal;
+            } 
+            else
+            {
+                finalPrice = originPrice - discPercentageInRupiah;
+            }
+            return finalPrice;
+        } 
+       
+
+       
+        
+    }
+}
